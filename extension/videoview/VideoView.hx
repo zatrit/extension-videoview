d09bcb6f6a456f27253a7e7bbbdba4b8c3154957
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-04-15 21:02:25 +0300

    Idc now about them

--- extension/videoview/VideoView.hx
+++ extension/videoview/VideoView.hx
@@ -21,33 +21,9 @@ class VideoView
 		_playVideo(path);
                 #end
         }
-
-	public static function pauseVideo():Void 
-        {
-                #if android
-		_pauseVideo();
-                #end
-        }
-
-	public static function resumeVideo():Void 
-        {
-                #if android
-		_resumeVideo();
-                #end
-        }
-
-	public static function seekVideoTo(msec:Int = 0):Void 
-        {
-                #if android
-		_seekVideoTo(msec);
-                #end
-        }
     
 	#if android
 	private static var _playVideo = JNI.createStaticMethod("extensions/videoview/VideoViewExtension", "playVideo", "(Ljava/lang/String;)V");
-	private static var _pauseVideo = JNI.createStaticMethod("extensions/videoview/VideoViewExtension", "pauseVideo", "()V");
-	private static var _resumeVideo = JNI.createStaticMethod("extensions/videoview/VideoViewExtension", "resumeVideo", "()V");
-	private static var _seekVideoTo = JNI.createStaticMethod("extensions/videoview/VideoViewExtension", "seekVideoTo", "(I)V");
 	private static var _callbackFunc = JNI.createStaticMethod("extensions/videoview/VideoViewExtension", "setCallback", "(Lorg/haxe/lime/HaxeObject;)V");
 	#end
 }
