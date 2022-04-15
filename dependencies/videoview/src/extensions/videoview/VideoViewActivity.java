5535c0c5ee6a8c9926389efee193fdce5fd6896f
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-04-15 19:20:04 +0300

    Offfffffff

--- dependencies/videoview/src/extensions/videoview/VideoViewActivity.java
+++ dependencies/videoview/src/extensions/videoview/VideoViewActivity.java
@@ -5,7 +5,6 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.VideoView;
 import org.haxe.lime.HaxeObject;
@@ -20,117 +19,77 @@ public class VideoViewActivity extends Activity {
 	public static String videoPath;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 
-		try {
-			Bundle extras = getIntent().getExtras();
-			videoPath = extras.getString(VideoViewExtension.EXTRA_VIDEOPATH);
-			callback = VideoViewExtension.callback;
+		Bundle extras = getIntent().getExtras();
+		videoPath = extras.getString(VideoViewExtension.EXTRA_VIDEOPATH);
+		callback = VideoViewExtension.callback;
 
-			if(Build.VERSION.SDK_INT >= 19) 
-			{
-				getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+		if(Build.VERSION.SDK_INT >= 19) 
+		{
+			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+		}
+		else if(Build.VERSION.SDK_INT >= 16) 
+		{
+			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+		}
+
+		setContentView(R.layout.videoview_activity);
+
+		videoView = (VideoView) findViewById(R.id.simpleVideoView);
+	}
+
+	@Override
+	protected void onDestroy(){
+		super.onDestroy();
+		videoView.start();
+	}
+
+	@Override
+	protected void onStart(){
+		super.onStart();
+
+		videoView.setVideoURI(Uri.parse(videoPath));
+		videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+			@Override
+			public void onCompletion(MediaPlayer mp) { 
+				callback.call("onCompletion", new Object[] {});
+				finish();
 			}
-			else if(Build.VERSION.SDK_INT >= 16) 
-			{
-				getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+		});
+		videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+			@Override
+			public boolean onError(MediaPlayer mp, int what, int extra) {
+				callback.call("onError", new Object[] {});
+				finish();
+				return false;
 			}
-
-			setContentView(R.layout.videoview_activity);
-
-			videoView = (VideoView) findViewById(R.id.simpleVideoView);
-		} catch (Exception e) {
-			Log.d("VideoViewActivity", e.toString());
-		}
+		});
+		videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+			@Override
+			public void onPrepared(MediaPlayer mp) { 
+				callback.call("onPrepared", new Object[] {});
+			}
+		});
+		videoView.start();
 	}
 
 	@Override
-	protected void onStart() {
-		try {
-			Uri uri = Uri.parse(videoPath);
-			videoView.setVideoURI(uri);
-			videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-				@Override
-				public void onCompletion(MediaPlayer mp) { 
-					callback.call("onCompletion", new Object[] {});
-					finish();
-				}
-			});
-			videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
-				@Override
-				public boolean onError(MediaPlayer mp, int what, int extra) {
-					callback.call("onError", new Object[] {});
-					finish();
-					return false;
-				}
-			});
-			videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-				@Override
-				public void onPrepared(MediaPlayer mp) { 
-					callback.call("onPrepared", new Object[] {});
-				}
-			});
-
-			videoView.start();
-		} catch (Exception e) {
-			Log.d("VideoViewActivity", e.toString());
-		}
+	protected void onStop(){
+		super.onStop();
+		videoView.stopPlayback();
 	}
 
 	@Override
-	protected void onDestroy() {
-		try {
-			videoView.stopPlayback();
-		} catch (Exception e) {
-			Log.d("VideoViewActivity", e.toString());
-		}
-	}
-
-	@Override
-	protected void onPause() {
-		try {
-			VideoViewActivity.videoView.pause();
-		} catch (Exception e) {
-			Log.d("VideoViewActivity", e.toString());
-		}
-	}
-
-	@Override
-	protected void onResume() {
-		try {
-			VideoViewActivity.videoView.resume();
-		} catch (Exception e) {
-			Log.d("VideoViewActivity", e.toString());
-		}
-	}
-
-	@Override
-	protected void onStop() {
-		try {
-			videoView.stopPlayback();
-		} catch (Exception e) {
-			Log.d("VideoViewActivity", e.toString());
-		}
-	}
-
-	@Override
-	public void onBackPressed() {
-		try {
-			callback.call("onCompletion", new Object[] {});
-			finish();
-		} catch (Exception e) {
-			Log.d("VideoViewActivity", e.toString());
-		}
+	public void onBackPressed(){
+		callback.call("onCompletion", new Object[] {});
+		finish();
 	}
 
 	@Override
 	public void finish(){
 		super.finish();
-		try {
-			videoView.stopPlayback();
-		} catch (Exception e) {
-			Log.d("VideoViewActivity", e.toString());
-		}
+		videoView.stopPlayback();
 	}
 }
